cb268a5d9d8e82521ebbff9fe5940660db574097
gr33kdude <gr33kdude@a1cfd482-cf27-0410-8d1c-7b035015a493>
2007-01-25 04:33:56 +0000

    the beginning

---
+++ trunk/lewis.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+	char *compound = malloc(127);
+	int sumValence = 0;
+	
+	scanf("%s", compound);	
+	printf("%s\n", compound);
+
+	return 0;
+}
+	
